8a8e2f871ed1ffc51a0b629880ceae3291b007b0
izilong <izilong@izilong.local>
2020-12-20 10:27:25 +0800

    tt 5

--- api/server.js
+++ api/server.js
@@ -1,4 +1,21 @@
-module.exports = (req, res) => {
-  const { query } = req;
-  res.send('hello world ++ ' + query.age);
-}
+const Koa = require('koa'),
+      router = require('koa-router')(),
+      static = require('koa-static'),
+      views = require('koa-views');
+
+const app = new Koa();
+
+app.use(views('views', {
+  extension: 'ejs'
+}))
+
+app.use(static(__dirname + '/static'));
+
+app.use('/home', async (ctx) => {
+  await ctx.render('home');
+})
+
+app.use(router.routes())
+   .use(router.allowedMethods());
+
+module.exports = app.callback();
